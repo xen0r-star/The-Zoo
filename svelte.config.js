2d5648fd0c81ee90f5ab9278c6cb4a1de1392fde
Florian <98989004+xen0r-star@users.noreply.github.com>
2025-08-13 23:57:05 +0200

    :sparkles: Add big chameleon

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,14 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: { 
+		adapter: adapter({
+			fallback: 'index.html',
+		}),
+		paths: {
+			base: "/TheZoo"
+		}
+	}
 };
 
 export default config;
