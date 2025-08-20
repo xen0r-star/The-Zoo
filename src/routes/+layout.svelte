796502d68d4baab6f48e3f7abe6433add9174ebe
Florian <98989004+xen0r-star@users.noreply.github.com>
2025-08-21 01:57:16 +0200

    :sparkles: Add new facts about chameleons to facts.json

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,10 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" rel="stylesheet" />
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Libre+Barcode+39&display=swap" rel="stylesheet">
 </svelte:head>
 
 {@render children?.()}
